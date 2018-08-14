36ed5ef097d84d436e52e8838343ea42bb4d4ce9
zhxh <zhxhcoder@gmail.com>
2018-08-14 16:53:02 +0800

    JniActivity

--- app/src/main/java/com/zhxh/xkotlin/JniActivity.kt
+++ app/src/main/java/com/zhxh/xkotlin/JniActivity.kt
@@ -2,13 +2,13 @@ package com.zhxh.xkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_jni.*
 
-class MainActivity : AppCompatActivity() {
+class JniActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_jni)
 
         // Example of a call to a native method
         sample_text.text = stringFromJNI()
